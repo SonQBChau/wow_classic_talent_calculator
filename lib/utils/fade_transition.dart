ea4f4e5c64e921f564f014c828891629fb2f4ad3
Son Chau <soncqb@gmail.com>
2019-08-26 10:32:51 -0500

    Create fade_transition.dart

---
+++ lib/utils/fade_transition.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomPageTransition extends PageRouteBuilder {
+  final Widget widget;
+  CustomPageTransition({this.widget})
+      : super(pageBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation) {
+          return widget;
+        }, transitionsBuilder: (BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        });
+}
